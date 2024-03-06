e7d94d7f4a9ee4630e60e103f481fa058b3c6559
Usuario <Usuario@10.245.42.127>
2024-03-06 15:03:09 +0100

    lehendabiziko commit

---
+++ HelloWorld/src/gui/HelloWorld.java
@@ -0,0 +1,46 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.util.HashMap;
+
+public class HelloWorld extends JFrame {
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HelloWorld frame = new HelloWorld();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelloWorld() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JLabel lblHelloWorld = new JLabel("Kaixo Mundua");
+		lblHelloWorld.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblHelloWorld.setBounds(115, 93, 225, 44);
+		contentPane.add(lblHelloWorld);
+	}
+}
