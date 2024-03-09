3d65dc968e07a56c59b2928c23c7002cb2ea0ff9
nicoibarburu <nicolasibarburus@gmail.com>
2024-03-09 10:59:09 +0100

    dataAccess added

---
+++ HelloWorld/src/dataAccess/DataAccess.java
@@ -0,0 +1,14 @@
+package dataAccess;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class DataAccess {
+	List<String> getTexts() {
+		return null; // hemen datubase atzipena inplementatu
+	}
+
+	HashMap<String, String> getTraductions(String msg) {
+		return null; // hemen datubase atzipena inplementatu
+	}
+}
